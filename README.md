45db62967e332f39eb52448a19369902e41e4b6d
skatz1990 <skatz1990@gmail.com>
2016-10-21 15:24:04 -0400

    Core commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # its-a-date-frontend
+
+its-a-date frontend project grants its-a-date a UI support
+
+https://github.com/bendorshai/its-a-date
--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var app = express();
 
 var PORT = 3000;
  
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + "/views/main.html");
-})
- 
-app.listen(PORT);
+//changing folders permissions to Public
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+// get index
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/views/main.html");
+});
+
+// listen
+var server = app.listen(PORT, function () {
+    console.log("Express server listening on port", PORT);
+});
